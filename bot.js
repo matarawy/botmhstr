3d239669d7cb8e3e22f9247982c9627a1b7aa3fe
mhstr <38856850+mhstr@users.noreply.github.com>
2018-07-22 20:33:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,7 +303,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('>bcall')){
- if (message.author.id !== '389090790984515594') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if (message.author.id !== '350408440566382592') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
  if(!message.author.id === '463781827086254083') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
