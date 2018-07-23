35158adb7bcf44c79fa7c63e7189e9394e12e7bc
mhstr <38856850+mhstr@users.noreply.github.com>
2018-07-22 18:02:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1004,50 +1004,53 @@ function getValue(key, array) {
   }
 }
 
- client.on('message', message => {
-	    var prefix = "!";
-              if(!message.channel.guild) return;
-    if(message.content.startsWith(prefix + 'bc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Morro BOT";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-    
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-    
-    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
-    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-  
-  var bc = new
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setTitle('Broadcast')
-       .addField('سيرفر', message.guild.name)
-       .addField('المرسل', message.author.username)
-       .addField('الرسالة', args)
-       .setThumbnail(message.author.avatarURL)
-       .setFooter(copy, client.user.avatarURL);
+client.on('message', message => {//By Codes , ' ّEpicEdiTeDّ#4968
+              if(!message.channel.guild) return;//By Codes , ' ّEpicEdiTeDّ#4968
+    var prefix = "!";//By Codes , ' ّEpicEdiTeDّ#4968
+    if(message.content.startsWith('!bc')) {//By Codes , ' ّEpicEdiTeDّ#4968
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للإدارة**').then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية لاستعمال هاذا الأمر** //By Codes , ' ّEpicEdiTeDّ#4968`ADMINISTRATOR`' );//By Codes , ' ّEpicEdiTeDّ#4968
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);//By Codes , ' ّEpicEdiTeDّ#4968
+    let copy = " Morro Bot";//By Codes , ' ّEpicEdiTeDّ#4968
+    let request = `Requested By ${message.author.username}`;//By Codes , ' ّEpicEdiTeDّ#4968
+    if (!args) return message.reply('**يجب عليك كتابة شيئ لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من الإرسال؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {//By Codes , ' ّEpicEdiTeDّ#4968
+    msg.react('✅')//By Codes , ' ّEpicEdiTeDّ#4968
+    .then(() => msg.react('❌'))//By Codes , ' ّEpicEdiTeDّ#4968
+    .then(() =>msg.react('✅'))//By Codes , ' ّEpicEdiTeDّ#4968
+ 
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;//By Codes , ' ّEpicEdiTeDّ#4968
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;//By Codes , ' ّEpicEdiTeDّ#4968
+          let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });//By Codes , ' ّEpicEdiTeDّ#4968
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });//By Codes , ' ّEpicEdiTeDّ#4968
+    reaction1.on("collect", r => {//By Codes , ' ّEpicEdiTeDّ#4968
+    message.channel.send(`**☑ |   لقد تم ارسال الرسالة لـ ${message.guild.members.size} عضوآ**`).then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968
+    message.guild.members.forEach(m => {//By Codes , ' ّEpicEdiTeDّ#4968
+    var bc = new//By Codes , ' ّEpicEdiTeDّ#4968
+       Discord.RichEmbed()//By Codes , ' ّEpicEdiTeDّ#4968
+       .setColor('RANDOM')//By Codes , ' ّEpicEdiTeDّ#4968
+       .setDescription(`البرودكاست :mega:
+**:shield: السيرفر : ** ${message.guild.name}
+** :thinking:  المرسل : ** ${message.author.username}
+**  الرسالة : ** ${args}
+ 
+ 
+ 
+ 
+        `)//By Codes , ' ّEpicEdiTeDّ#4968
+         .setTimestamp()//By Codes , ' ّEpicEdiTeDّ#4968
+         .setFooter('Morro Bot' , 'https://cdn.discordapp.com/avatars/466799323263401986/ba5ace1dcfb8711c6eba08f643174a7f.jpg?size=128')
     m.send({ embed: bc })
-    msg.delete();
+    msg.delete();//By Codes , ' ّEpicEdiTeDّ#4968
+    })//By Codes , ' ّEpicEdiTeDّ#4968
+    })//By Codes , ' ّEpicEdiTeDّ#4968
+    reaction2.on("collect", r => {//By Codes , ' ّEpicEdiTeDّ#4968
+    message.channel.send(`**تم الغاء البرودكاست :x:.**`).then(m => m.delete(5000));//By Codes , ' ّEpicEdiTeDّ#4968//By Codes , msg.delete();//By Codes , ' ّEpicEdiTeDّ#496
     })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
+    })//By Codes , ' ّEpicEdiTeDّ#4968
     }
-    });
-
+    });//By Codes , ' ّEpicEdiTeDّ#4968//By Codes , ' ّEpicEdiTeDّ#4968
+ 
+//By Codes , ' ّEpicEdiTeDّ#4968
 let points = {};
 const type = [
     {
