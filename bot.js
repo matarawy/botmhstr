6b65c43198b0f908cacb6921f3c30a502e04a6e1
MHSTRBOTT <38856850+MHSTRBOTT@users.noreply.github.com>
2018-07-27 14:06:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1114,7 +1114,24 @@ client.on('message', msg => {
     }
 };
 });
-
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '472503640154963968').setName("W");
+client.channels.find('id', '472503640154963968').setName("We");
+client.channels.find('id', '472503640154963968').setName("Wel");
+client.channels.find('id', '472503640154963968').setName("Welc");
+client.channels.find('id', '472503640154963968').setName("Welco");
+client.channels.find('id', '472503640154963968').setName("Welcom");
+client.channels.find('id', '472503640154963968').setName("Welcome");
+client.channels.find('id', '472503640154963968').setName("Welcome T");
+client.channels.find('id', '472503640154963968').setName("Welcome To");
+client.channels.find('id', '472503640154963968').setName("Welcome To H");
+client.channels.find('id', '472503640154963968').setName("Welcome To Ha");
+client.channels.find('id', '472503640154963968').setName("Welcome To Hal");
+client.channels.find('id', '472503640154963968').setName("Welcome To Halo");
+client.channels.find('id', '472503640154963968').setName("Welcome To Halos7");
+  }, 3000);
+});
 client.on('message', message => { 
 	var prefix = "!";
  let args = message.content.split(' ').slice(1);
@@ -2604,23 +2621,6 @@ client.channels.find('id', '472190387394707476').setName("Welcome To Sliver");
   }, 3000);
 });
 
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '472503640154963968').setName("W");
-client.channels.find('id', '472503640154963968').setName("We");
-client.channels.find('id', '472503640154963968').setName("Wel");
-client.channels.find('id', '472503640154963968').setName("Welc");
-client.channels.find('id', '472503640154963968').setName("Welco");
-client.channels.find('id', '472503640154963968').setName("Welcom");
-client.channels.find('id', '472503640154963968').setName("Welcome");
-client.channels.find('id', '472503640154963968').setName("Welcome T");
-client.channels.find('id', '472503640154963968').setName("Welcome To");
-client.channels.find('id', '472503640154963968').setName("Welcome To H");
-client.channels.find('id', '472503640154963968').setName("Welcome To Ha");
-client.channels.find('id', '472503640154963968').setName("Welcome To Hal");
-client.channels.find('id', '4472503640154963968').setName("Welcome To Halo");
-client.channels.find('id', '472503640154963968').setName("Welcome To Halos7");
-  }, 3000);
-});
+
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
