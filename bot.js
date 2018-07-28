c4ba6b6a94feae4476e65a3d52162f1560affa42
MHSTRBOTT <38856850+MHSTRBOTT@users.noreply.github.com>
2018-07-28 08:51:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2605,47 +2605,8 @@ client.channels.find('id', '472190387394707476').setName("Welcome To Sliver");
   }, 3000);
 });
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("471679292783329280")// ايدي السيرفر
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 
 
 
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("472788956019163147");// ايدي الروم
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("437717227244879882")// ايدي السيرفر
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`**ارحب يا __<@${member.id}>__ في سيرفر __${member.guild.name}__\nتم دعوته من قبل  ${Invite.inviter}\nالرابط اللي دخل منه **: https://discord.gg/${Invite.code}`)
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
